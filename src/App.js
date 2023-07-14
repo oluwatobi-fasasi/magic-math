ea5f81a3c8cf34f6a738b749add41018b1952fa2
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-14 05:52:16 -0400

    Update App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Calculator from './components/Calculator';
+import Quotes from './components/quotes';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <Calculator />
+      <div className="bg-orange-300 p-7 text-center text-base">
+        <Quotes />
+      </div>
+    </>
   );
 }
 
