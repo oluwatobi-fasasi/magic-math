cc1431ffeca70fb907b413c182091d514783e42d
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-14 05:51:57 -0400

    Update Screen size

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -5,7 +5,7 @@ Text.propTypes = {
 };
 function Screen({ show }) {
   return (
-    <div className="bg-slate-500 h-80 flex flex-col justify-center">
+    <div className="bg-slate-500 h-48 flex flex-col justify-center">
       <h1 className="text-6xl self-end text-white">{show.next || show.total}</h1>
     </div>
   );
