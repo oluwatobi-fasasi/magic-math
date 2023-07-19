9e3fed5f2a8e51aaa8c4761bb02002e303a9c34a
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-19 01:09:37 -0400

    Add homepage

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="text-left pl-6 mx-60 my-52 text-2xl">
+      <p className="text-4xl my-4">Welcome to our page</p>
+      <div className="my-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis,
+        eveniet! Blanditiis, perferendis! Eaque ad ipsam autem cum tempore
+        consequatur praesentium? Facilis vitae praesentium assumenda placeat a.
+        Officia consequuntur aspernatur quo optio? Reiciendis a similique vitae,
+        vel blanditiis temporibus quo repudiandae voluptatem eligendi architecto
+        ipsa veritatis itaque voluptate quibusdam repellendus porro modi
+        pariatur obcaecati quos voluptatibus molestiae atque aspernatur illo.
+      </div>
+      <div className="pt-8 tracking-wide">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis,
+        eveniet! Blanditiis, perferendis! Eaque ad ipsam autem cum tempore
+        consequatur praesentium? Facilis vitae praesentium assumenda placeat a.
+        Officia consequuntur aspernatur quo optio? Reiciendis a similique vitae,
+        vel blanditiis temporibus quo repudiandae voluptatem eligendi architecto
+        ipsa veritatis itaque voluptate quibusdam repellendus porro modi
+        pariatur obcaecati quos voluptatibus molestiae atque aspernatur illo.
+      </div>
+    </div>
+  );
+}
